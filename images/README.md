cab98a9f459337d8000dedc5b746ca9ec4108451
coffeemakr <git@unstable.ch>
2016-03-03 14:24:31 +0100

    Script and updated readme

--- images/README.md
+++ images/README.md
@@ -5,14 +5,19 @@ According to my tests you have to be in a Swisscom (customer) network to
 download firmware images .  
 
 ### Versions
+
 * [Current Version](http://rmsdl.bluewin.ch/pirelli/Vx226N1_current_epicentro_release.sig)
 * [6.14.06 (Vx226x1_61406.sig)](http://rmsdl.bluewin.ch/pirelli/Vx226x1_61406.sig)
 * [????](http://rmsdl.bluewin.ch/pirelli/Vx226N1_50033.rmt)
 
-## In depth analysis
-* [6.14.06](image_6.14.06.md)
+## Version Info
+
+| Magic | Version | Date | Download Link |
+| ----- | ------- | ---- | ------------- |
+| yIMG  | [6.14.06R](image_6.14.06.md) | 2015-09-23 18:00 | [link](http://rmsdl.bluewin.ch/pirelli/Vx226x1_61406.sig) |
+
 
 ## Thanks
-A big thank you to [Antoine Albertelli](https://github.com/antoinealb) who made
+A thank you to [Antoine Albertelli](https://github.com/antoinealb) who made
 a great [github repo](https://github.com/antoinealb/swisscom_centro_grande) on
 his findings.
---
+++ images/scripts/imageinfo.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import print_function
+
+import sys
+import os
+
+class Image(object):
+
+    def __init__(self, path):
+        self.path = path
+        self.filename = os.path.split(self.path)[1]
+        with open(self.path, 'rb') as imagefile:
+            self.magic = imagefile.read(4)
+            imagefile.seek(48)
+            self.version = imagefile.read(16).strip(b'\x00')
+            imagefile.seek(64)
+            self.date = imagefile.read(16).strip(b'\x00')
+
+def main():
+    if len(sys.argv) < 2:
+        print("imageinfo.py FILE")
+        sys.exit(1)
+
+    image = None
+    try:
+        image = Image(sys.argv[1])
+    except Exception as e:
+        #print(e)
+        raise
+        sys.exit(1)
+
+    print("File    : %s" % image.filename)
+    print("Magic   : %s" % image.magic)
+    print("Version : %s" % image.version)
+    print("Data    : %s" % image.date)
+
+
+if __name__ == '__main__':
+    main()
